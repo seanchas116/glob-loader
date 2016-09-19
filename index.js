91b3f6337add88cc03947beacc33233aaa81cbc3
Joscha Feth <joscha@feth.com>
2016-09-20 00:12:22 +1000

    feat: warn if there are no matches for the glob

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ var path = require("path");
 module.exports = function (content, sourceMap) {
   this.cacheable && this.cacheable();
   var resourceDir = path.dirname(this.resourcePath);
-  var files = glob.sync(content.trim(), {
+  var pattern = content.trim();
+  var files = glob.sync(pattern, {
     cwd: resourceDir
   });
+
+  if (!files.length) {
+    this.emitWarning('Did not find anything for glob "' + pattern + '" in directory "' + resourceDir + '"');
+  }
 
   return "module.exports = [\n" + files.map(function (file) {
     return "  require(" + JSON.stringify(file) + ")"
