33778a350c4051f2782b65ad3a88ca0c6e80e850
Joscha Feth <joscha@feth.com>
2016-09-20 00:02:53 +1000

    style: remove unused loader-utils

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 "use strict";
 
-var loaderUtils = require("loader-utils");
 var glob = require("glob");
 var path = require("path");
 
 module.exports = function (content, sourceMap) {
-  // var query = loaderUtils.parseQuery(this.query);
   var resourceDir = path.dirname(this.resourcePath);
   var files = glob.sync(content.trim(), {
     cwd: resourceDir
